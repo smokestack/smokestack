3d5470c90e1ff46c075bbd7523e98b01f9701cea
Gabor Liptak <gliptak@gmail.com>
2009-12-08 21:47:29 -0500

    Add main site and common module.

---
+++ smokestack-common/src/test/java/net/sourceforge/smokestack/exception/NotYetImplementedExceptionTest.java
@@ -0,0 +1,36 @@
+package net.sourceforge.smokestack.exception;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class NotYetImplementedExceptionTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testNotYetImplementedExceptionStringThrowable() {
+        NotYetImplementedException e=new NotYetImplementedException("some message", new Exception());
+        assertThat("some message", is(e.getMessage()));
+	}
+
+}
